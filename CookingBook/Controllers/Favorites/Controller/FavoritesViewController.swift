fed501f2757a010ea09ae29e7b89d86a16ac6fb2
Mirasque <obscure1321@gmail.com>
2023-09-01 23:30:44 +0300

    added tableView & custom cell

--- CookingBook/Controllers/Favorites/Controller/FavoritesViewController.swift
+++ CookingBook/Controllers/Favorites/Controller/FavoritesViewController.swift
@@ -28,22 +28,37 @@ class FavoritesViewController: UIViewController {
         let element = UITableView()
         element.translatesAutoresizingMaskIntoConstraints = false
         element.backgroundColor = .systemGreen
+        element.alwaysBounceVertical = true
         return element
     }()
+    
+// MARK: - properties
+    var favouritesArray = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     
 // MARK: - life cycle funcs
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        setUpView()
-        setConstraints()
+        setUp()
     }
     
 // MARK: - flow funcs
+    private func setUp() {
+        view.backgroundColor = .white
+        setUpView()
+        setDelegate()
+        setConstraints()
+    }
+    
     private func setUpView() {
         view.addSubview(labelView)
         labelView.addSubview(mainLabel)
         view.addSubview(recipeTable)
+    }
+    
+    private func setDelegate() {
+        recipeTable.delegate = self
+        recipeTable.dataSource = self
+        recipeTable.register(TableViewCell.self, forCellReuseIdentifier: "TableViewCell")
     }
     
     private func setConstraints() {
@@ -63,5 +78,23 @@ class FavoritesViewController: UIViewController {
             recipeTable.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
             recipeTable.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
         ])
+    }
+}
+
+extension FavoritesViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 282.0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favouritesArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = recipeTable.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as? TableViewCell else {
+            return UITableViewCell()
+        }
+        cell.setUpCell()
+        return cell
     }
 }
