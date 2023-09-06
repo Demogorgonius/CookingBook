04d34f0043558be5af98fef3672066b0037b9e3a
Eremenok Elizaveta <eremeonock.el@yandex.ru>
2023-09-06 15:34:58 +0300

    Create Recepie Information ViewController

---
+++ CookingBook/Controllers/RecipeInformation/RecipeInformationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RecipeInformationViewController.swift
+//  CookingBook
+//
+//  Created by Elizaveta Eremyonok on 06.09.2023.
+//
+
+import UIKit
+
+class RecipeInformationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
